b4edbbb25eac9cae627b263a09f412599fee8f10
ksolomon <ksolomon@lin33005536.corp.capgemini.com>
2024-09-22 21:34:51 +0530

    selenium upgrade

--- src/main/java/com/learn/Section8/base.java
+++ src/main/java/com/learn/Section8/base.java
@@ -1,13 +1,21 @@
 package com.learn.Section8;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,12 +27,33 @@ public class base {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
 		//expected Array
 		String [] itemsNeeded= {"Cucumber","Brocolli","Beetroot","Carrot"};
 		driver.get("https://rahulshettyacademy.com/seleniumPractise");
 		
+		addItems(driver, itemsNeeded);
+		driver.findElement(By.xpath("//img[@alt=\"Cart\"]")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+//        WebDriverWait w=new WebDriverWait(driver, Duration.ofSeconds(5));
+//		w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"promoBtn\"]")));
+		driver.findElement(By.xpath("//button[@class=\"promoBtn\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div/button ")).click();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void addItems(WebDriver driver,String [] itemsNeeded) {
 		List<WebElement> products= driver.findElements(By.cssSelector("h4.product-name"));
 		int j=0;
 		for(int i=0;i<products.size();i++) {
@@ -47,7 +76,25 @@ public class base {
 		}
 		System.out.println("Cart Added is "+i);
 		}
-	//driver.close();	
+	}
+	
+	public void fluentWait(WebDriver driver) {
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		driver.findElement(By.cssSelector("[id= 'start'] button")).click();
+		Wait<WebDriver>wait= new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+		WebElement foo= wait.until(new Function<WebDriver, WebElement>() {
+
+			@Override
+			public WebElement apply(WebDriver driver) {
+				if(driver.findElement(By.cssSelector("[id='finish] h4")).isDisplayed()) {
+					return driver.findElement(By.cssSelector("[id='finish] h4"));
+				}
+				else {
+				return null;
+				}
+			}
+		});
+		System.out.println(driver.findElement(By.cssSelector("[id='finish] h4")).getText());
 	}
 }
 
